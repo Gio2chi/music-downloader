1aff70e9906783f69840054e05ca8975ebd8f7c1
Gio2chi <gio.angaroni@gmail.com>
2025-10-25 17:08:25 +0000

    compiled cleanup

--- dist/ServerInstance.d.ts
+++
@@ -1,3 +0,0 @@
-import passport from "passport";
-declare const app: import("express-serve-static-core").Express;
-export { app, passport };
--- dist/ServerInstance.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import session from "express-session";
-import passport from "passport";
-const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(session({ secret: "supersecret", resave: false, saveUninitialized: false }));
-app.use(passport.initialize());
-export { app, passport };
